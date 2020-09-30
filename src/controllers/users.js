6e639c240c6666cb43fda224091d56ad0bb41182
yyy-pixel <pnksm009@gmail.com>
2020-09-30 21:50:50 +0700

    update

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -9,60 +9,67 @@ const fs = require('fs')
 
 const users = {
     register: async (req, res) => {
-        try {
-            const body = req.body
-            const password = req.body.password
-            const salt = await bcrypt.genSalt(10)
-            const generate = await bcrypt.hash(password, salt)
-            const img = "default.jpg"
+        // try {
+            
+        // } catch (error) {
+        //     failed(res, [], 'Internal Server Error')
+        // }
 
-            const data = {
-                username: body.username,
-                email: body.email,
-                password: generate
-            }
-            userModel.register(data, img).then((result) => {
-                const token = jwt.sign({ email: body.email }, PRIVATEKEY)
-                const output = `
-                    <center><h3>Hello ${req.body.email}</h3>
-                    <h3>Thank you for registration</h3>
-                    <p>You can confirm your email by clicking the link below <br> <a href="http://18.207.247.9:3005/users/active/${token}">Activation</a></p></center>
-                    `
-                let transporter = nodemailer.createTransport({
-                    host: 'smtp.gmail.com',
-                    port: 587,
-                    secure: false,
-                    requireTLS: true,
-                    auth: {
-                        user: env.EMAIL,
-                        pass: env.PASSWORD_EMAIL
-                    }
-                })
+        const body = req.body
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const generate = await bcrypt.hash(password, salt)
+        const img = "default.jpg"
 
-                let Mail = {
-                    from: '"Ankasa" <testerweb533@gmail.com>',
-                    to: req.body.email,
-                    subject: "Verification Email",
-                    text: "Plaintext version of the message",
-                    html: output
-                }
-
-                transporter.sendMail(Mail)
-                success(res, result, 'Please check your email to activation')
-            }).catch((err) => {
-                if (err.message = 'Duplicate entry') {
-                    failed(res, [], 'Email Already Exist')
-                } else {
-                    failed(res, [], err.message)
+        const data = {
+            username: body.username,
+            email: body.email,
+            password: generate
+        }
+        userModel.register(data, img).then((result) => {
+            const token = jwt.sign({ email: body.email }, PRIVATEKEY)
+            const output = `
+                <center><h3>Hello ${req.body.email}</h3>
+                <h3>Thank you for registration</h3>
+                <p>You can confirm your email by clicking the link below <br> <a href="http://18.207.247.9:3005/users/active/${token}">Activation</a></p></center>
+                `
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false,
+                requireTLS: true,
+                auth: {
+                    user: env.EMAIL,
+                    pass: env.PASSWORD_EMAIL
                 }
             })
-        } catch (error) {
-            failed(res, [], 'Internal Server Error')
-        }
+
+            let Mail = {
+                from: '"Ankasa" <testerweb533@gmail.com>',
+                to: req.body.email,
+                subject: "Verification Email",
+                text: "Plaintext version of the message",
+                html: output
+            }
+
+            transporter.sendMail(Mail)
+            success(res, result, 'Please check your email to activation')
+        }).catch((err) => {
+            if (err.message = 'Duplicate entry') {
+                failed(res, [], 'Email Already Exist')
+            } else {
+                failed(res, [], err.message)
+            }
+        })
     },
     active: (req, res) => {
-        try {
-            const token = req.params.token
+        // try {
+            
+        // } catch (error) {
+        //     failed(res, [], 'Internal Server Error')
+        // }
+
+        const token = req.params.token
             jwt.verify(token, PRIVATEKEY, (err, decode) => {
                 if (err) {
                 failed(res, [], 'Failed authorization!')
@@ -73,112 +80,110 @@ const users = {
                         res.render('index', { email })
                     }).catch(err => {
                         failed(res, [], err.message)
-                    })
-                }
-            })
-        } catch (error) {
-            failed(res, [], 'Internal Server Error')
-        }
+                })
+            }
+        })
     },
     login: async (req, res) => {
-        try {
-            const body = req.body
-            const usersData = {
-                username: body.username,
-                password: body.password
-            }
-            userModel.login(usersData).then(async (result) => {
-                const results = result[0]
-                const id = results.id
+        // try {
+            
+        // } catch (error) {
+        //     failed(res, [], 'Internal Server Error')
+        // }
 
-                if (!results) {
-                    failed(res, [], 'Email not registered, Please register!')
-                } else {
-                    const password = results.password
+        const body = req.body
+        const usersData = {
+            username: body.username,
+            password: body.password
+        }
+        
+        userModel.login(usersData).then(async (result) => {
+            const results = result[0]
+            const id = results.id
 
-                    const isMatch = bcrypt.compareSync(usersData.password, password)
+            if (!results) {
+                failed(res, [], 'Email not registered, Please register!')
+            } else {
+                const password = results.password
 
-                    if (isMatch) {
-                        if (results.status === 1) {
-                            const dataUser = {
-                                username: results.username,
-                                role: results.role
-                            }
+                const isMatch = bcrypt.compareSync(usersData.password, password)
 
-                            const refreshToken = jwt.sign(dataUser, REFRESHTOKEN)
-                            const token = newerToken(dataUser)
+                if (isMatch) {
+                    if (results.status === 1) {
+                        const dataUser = {
+                            username: results.username,
+                            role: results.role
+                        }
 
-                            if (results.refreshToken === null) {
-                                userModel.updateRefreshToken(refreshToken, id).then((result) => {
-                                    const data = {
-                                        token,
-                                        refreshToken: refreshToken
-                                    }
-                                    tokenResult(res, data, 'Login success')
-                                }).catch((err) => {
-                                    failed(res, [], err.message)
-                                })
-                            } else {
+                        const refreshToken = jwt.sign(dataUser, REFRESHTOKEN)
+                        const token = newerToken(dataUser)
+
+                        if (results.refreshToken === null) {
+                            userModel.updateRefreshToken(refreshToken, id).then((result) => {
                                 const data = {
                                     token,
                                     refreshToken: refreshToken
                                 }
-                                tokenResult(res, data, 'Login successful')
-                            }
+                                tokenResult(res, data, 'Login success')
+                            }).catch((err) => {
+                                failed(res, [], err.message)
+                            })
                         } else {
-                            failed(res, [], 'Activation needed!')
+                            const data = {
+                                token,
+                                refreshToken: refreshToken
+                            }
+                            tokenResult(res, data, 'Login successful')
                         }
                     } else {
-                        failed(res, [], 'Password is wrong')
+                        failed(res, [], 'Activation needed!')
                     }
-                }
-            }).catch((err) => {
-                if (err.message === `Cannot read property 'id' of undefined`) {
-                    failed(res, [], 'Username is wrong')
                 } else {
-                    failed(res, [], err.message)
+                    failed(res, [], 'Password is wrong')
                 }
-            })
-        } catch (error) {
-            failed(res, [], 'Internal Server Error')
-        }
+            }
+        }).catch((err) => {
+            if (err.message === `Cannot read property 'id' of undefined`) {
+                failed(res, [], 'Username is wrong')
+            } else {
+                failed(res, [], err.message)
+            }
+        })
     },
     updateData: (req, res) => {
-        try {
-            upload.single('image')(req, res, (err) => {
-                if (err) {
-                    if (err.code === 'LIMIT_FILE_SIZE') {
-                        failed(res, [], 'File size max 2Mb')
-                    } else {
-                        failed(res, [], err)
-                    }
-                } else {
-                    const id = req.params.id
-                    userModel.getDetail(id)
-                    .then((result) => {
-                        const oldImg = result[0].image
-                        body.image = !req.file ? oldImg : req.file.filename
+        // try {
+            
+        // } catch (error) {
+        //     failed(res, [], 'Internal Server Error')
+        // }
 
-                        if (body.image !== oldImg) {
-                            if (oldImg !== 'default.jpg') {
-                                fs.unlink(`src/upload/${oldImg}`, (err) => {
-                                    if (err) {
-                                        failed(res, [], err.message)
-                                    } else {
-                                        userModel.updateAllData(body, id).then((result) => {
-                                            success(res, result, 'Update data success')
-                                        }).catch((err) => {
-                                            failed(res, [], err.message)
-                                        })
-                                    }
-                                })
-                            } else {
-                                userModel.updateAllData(body, id).then((result) => {
-                                    success(res, result, 'Update data success')
-                                }).catch((err) => {
+        upload.single('image')(req, res, (err) => {
+            if (err) {
+                if (err.code === 'LIMIT_FILE_SIZE') {
+                    failed(res, [], 'File size max 2Mb')
+                } else {
+                    failed(res, [], err)
+                }
+            } else {
+                const id = req.params.id
+                userModel.getDetail(id)
+                .then((result) => {
+                    const oldImg = result[0].image
+                    body.image = !req.file ? oldImg : req.file.filename
+
+                    if (body.image !== oldImg) {
+                        if (oldImg !== 'default.jpg') {
+                            fs.unlink(`src/upload/${oldImg}`, (err) => {
+                                if (err) {
                                     failed(res, [], err.message)
-                                })
-                            }
+                                } else {
+                                    userModel.updateAllData(body, id).then((result) => {
+                                        success(res, result, 'Update data success')
+                                    }).catch((err) => {
+                                        failed(res, [], err.message)
+                                    })
+                                }
+                            })
                         } else {
                             userModel.updateAllData(body, id).then((result) => {
                                 success(res, result, 'Update data success')
@@ -186,15 +191,19 @@ const users = {
                                 failed(res, [], err.message)
                             })
                         }
-                    })
-                    .catch((err) => {
-                        failed(res, [], err.message)
-                    })
-                }
-            })
-        } catch (error) {
-            failed(res, [], 'Internal Server Error')
-        }
+                    } else {
+                        userModel.updateAllData(body, id).then((result) => {
+                            success(res, result, 'Update data success')
+                        }).catch((err) => {
+                            failed(res, [], err.message)
+                        })
+                    }
+                })
+                .catch((err) => {
+                    failed(res, [], err.message)
+                })
+            }
+        })
     },
 
     requestToken: (req, res) => {
