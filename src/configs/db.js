fbc10a08c78208b1775027a0067917d1da476134
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2020-09-30 16:33:05 +0700

    env.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2')
-const { DB_HOST, DB_USER, DB_NAME } = require('../helpers/env')
+const { DB_HOST, DB_USER, DB_NAME, DB_PASS } = require('../helpers/env')
 
 const connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
+    password: DB_PASS,
     database: DB_NAME,
     dateStrings: "date",
 })
--- src/helpers/env.js
+++ src/helpers/env.js
@@ -5,6 +5,7 @@ module.exports = {
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
     DB_NAME: process.env.DB_NAME,
+    DB_PASS: process.env.DB_PASS,
     PRIVATEKEY: process.env.PRIVATEKEY,
     REFRESHTOKEN: process.env.REFRESHTOKEN,
     EMAIL: process.env.EMAIL,
