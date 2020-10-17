b706c89a893bec54229ae27a96ae270e08843af1
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2020-10-17 14:25:54 +0700

    repairing all

--- src/routes/cities.js
+++ src/routes/cities.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const { admin, authentication, authorization } = require('../helpers/auth')
 
 router
-    .get('/getAll', authentication, authorization, citiesController.getAll)
-    .get('/getDetail/:idcities', authentication, authorization, citiesController.getDetail)
+    .get('/getAll', citiesController.getAll)
+    .get('/getDetail/:idcities', citiesController.getDetail)
     .post('/insert', authentication, authorization, admin, citiesController.insert)
     .patch('/update/:idcities', authentication, authorization, admin, citiesController.update)
     .delete('/delete/:idcities', authentication, authorization, admin, citiesController.delete)
